59466895db22bc8d9c5d23cbe6cf2e4fbe279d2b
yuenkm40 <yuenkm40@gmail.com>
2022-05-30 11:14:19 +0800

    designed events page

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import './events.scss'
+import EventPic from '../assets/women.png'
+import Connect from '../assets/connect.jpg'
 export default function Events() {
   return (
     <div className="events" id="events">
-      events
+    <div className="top-banner">
+    <img src={EventPic} alt=""></img>
+    <h1>Attend life changing events. Buy tickets.</h1>
+    </div>
+
+    <div className="bottom">
+    <h1>Upcoming Events</h1>
+    <div className="eventList">
+    <img src={Connect} alt=""></img>
+    <img src={Connect} alt=""></img>
+    <img src={Connect} alt=""></img>
+    <img src={Connect} alt=""></img>
+    </div>
+    </div>
     </div>
   )
 }
