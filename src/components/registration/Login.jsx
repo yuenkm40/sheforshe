0d21f9c4078dc8e65d36394ff2b561a01e760de3
yuenkm40 <yuenkm40@gmail.com>
2022-05-30 17:45:19 +0800

    added login and signup pages

--- src/components/registration/Login.jsx
+++ src/components/registration/Login.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './login.scss'
 import pic from "../assets/login.png"
+import Auth from "./Auth"
+
 export default function Login() {
+
   return (
     <div className="login">
       <div className="left">
       <h1>Welcome to SheforShe. Start your journey here.</h1>
           <img src ={pic} alt=""></img>
       </div>
-      <div className="right"></div>
+      <div className="right">
+          <Auth/>
+      </div>
     </div>
   )
 }
